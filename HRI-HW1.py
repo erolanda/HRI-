7274f3079198b7496e4be53db9288adaedf819e2
erolanda <elmaildemlanda@gmail.com>
2015-10-06 19:08:09 -0500

    Now it has bluetooth support

--- HRI-HW1.py
+++ HRI-HW1.py
@@ -1,16 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Editor de Spyder
-
-Este es un archivo temporal
-"""
-
 import MySQLdb as db
 import threading
+import bluetooth
+from random import randint
+
+blacklist=[]
 
 def queryDataBase():
     global result
-    threading.Timer(5.0, queryDataBase).start()
+    threading.Timer(120.0, queryDataBase).start()
     HOST = "45.55.21.26"
     PORT = 3306
     USER = "root"
@@ -21,7 +18,7 @@ def queryDataBase():
         dbhandler = connection.cursor()
         dbhandler.execute("SELECT * from datos")
         result = dbhandler.fetchall()
-        print "Datos actualizados"
+        print "Base de datos actualizada"
     except Exception as e:
         print e
     
@@ -30,8 +27,35 @@ def queryDataBase():
         return result
 
 def bluetoothScann():
-    #TODO    
-    
+    threading.Timer(30.0, bluetoothScann).start()
+    print "Realizando escaneo bluetooth"
+    nearby_devices = bluetooth.discover_devices(duration=8, lookup_names=True, flush_cache=True, lookup_class=False)
+    print "Escaneo terminado"
+    available_devices = []
+    for item in nearby_devices:
+        for element in result:
+            if item[0] == element[5]:
+                available_devices.append((item[0],element[0]))
+    sayHi(available_devices)
+
+def sayHi(available_devices):
+    number_devices = len(available_devices) - 1
+    index = randint(0,number_devices)
+    print "Saludar a usuario con MAC %s" % (available_devices[index][0])
+    if available_devices[index][0] not in blacklist: 
+        print "Saludando ..."
+        blacklist.append(available_devices[index][0])
+        print available_devices[index][0]
+        print "Usuario agregado a blacklist para no repetir saludo"
+        return 0
+    print "Usuario en blacklist, buscar otro usuario..."
+    if (len(blacklist) == len(available_devices)):
+        print "Ya no hay usuarios para saludar"
+        return 0
+    else:
+        sayHi(available_devices)
+    return 0
+
+
 queryDataBase()
-for item in result:
-    print item
+bluetoothScann()
